eac517a0357952dfca8ecd1fc4a4ba04af2f43f4
Torsten Krause <tk@markenwerk.net>
2015-10-13 17:29:40 +0200

    verbosified copyright notice

--- src/main/java/net/markenwerk/utils/mail/dkim/DkimException.java
+++ src/main/java/net/markenwerk/utils/mail/dkim/DkimException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Torsten Krause.
+ * Copyright (c) 2015 Torsten Krause, Markenwerk GmbH.
  * 
  * This file is part of 'A DKIM library for JavaMail', hereafter
  * called 'this library', identified by the following coordinates:
