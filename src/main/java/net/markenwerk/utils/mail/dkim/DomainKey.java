460c525f418fd44650f6ae484e837b3c3e9d3218
Torsten Krause <tk@markenwerk.net>
2015-10-14 15:05:33 +0200

    added license info

--- src/main/java/net/markenwerk/utils/mail/dkim/DomainKey.java
+++ src/main/java/net/markenwerk/utils/mail/dkim/DomainKey.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2015 Torsten Krause, Markenwerk GmbH.
+ * 
+ * This file is part of 'A DKIM library for JavaMail', hereafter
+ * called 'this library', identified by the following coordinates:
+ * 
+ *    groupID: net.markenwerk
+ *    artifactId: utils-mail-dkim
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ * 
+ * See the LICENSE and NOTICE files in the root directory for further
+ * information.
+ */
 package net.markenwerk.utils.mail.dkim;
 
 import java.io.IOException;
